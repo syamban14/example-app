4ebcad8f049be6285e5bd5022e38e3877d1ab4b1
syamban14 <syamsulikj.48@gmail.com>
2022-07-22 15:15:09 +0700

    Menambahkan Auth ke Docker

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -5,18 +5,18 @@
 	<title>Halaman Beranda</title>
 </head>
 <body>
-<nav>
-<ul>
-	<li>
-		<span><a href="/">Home</a></span>
-	</li>
-	<li>
-		<span><a href="profile.php">Profile</a></span>
-	</li>
-	<li>
-		<span><a href="contact-form.php">Contact</a></span>
-	</li>
-</ul>
-</nav>
+	<nav>
+		<ul>
+			<li>
+				<span><a href="/">Home</a></span>
+			</li>
+			<li>
+				<span><a href="profile.php">Profile</a></span>
+			</li>
+			<li>
+				<span><a href="contact-form.php">Contact</a></span>
+			</li>
+		</ul>
+	</nav>
 </body>
 </html>
